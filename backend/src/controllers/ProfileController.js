5069753d79977095a8f8a6e003a6384efd025b1a
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-03-28 13:17:02 -0300

    tires all the logic of the route file and creates the controllers for each entity

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,15 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(request, response) {
+        const { authentication: pk_ong } = request.headers;
+
+        const incidents = await connection.table('incidents')
+            .select()
+            .where('ong_id', pk_ong);
+
+        console.log(incidents);
+        return response.json( {incidents} );
+    },
+
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,32 +1,21 @@
 const express = require('express');
-const crypto = require('crypto');
 
-const connection = require('./database/connection');
+const SessionController = require('./controllers/SessionController');
+const ProfileController = require('./controllers/ProfileController');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
+routes.post('/session', SessionController.create);
 
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection.select().from('ongs');
+routes.get('/profile', ProfileController.index);
 
-    return response.json(ongs);
-});
+routes.get('/ongs', OngController.index );
+routes.post('/ongs', OngController.create);
 
-routes.post('/ongs', async (request, response) => {
-    const { name, email, whatsapp, city, uf } = request.body;
-
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    })
-
-    return response.json({ id });
-});
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
