88ff507a8de49eecafbc12fd6589ed50fa32ff1c
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-03-31 09:21:41 -0300

    handleLogin is done to validate access to the profile page and on the profile page to return the cases of that logged ONG

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
+
 
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 function Login() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try {
+            const response = await api.post('/session', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        } catch(error) {
+            alert('Falha no login, tente novamente.')
+        }
+
+    }
+
     return (
         <div className="login-container">
             <section className="form">
                 <img src={logoImg} alt="Logo" />
             
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu login</h1>
 
-                    <input placeholder="Sua ID" />
+                    <input 
+                        placeholder="Sua ID" 
+                        value={id}
+                        onChange={e => setId(e.target.value)}    
+                    />
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register" className="back-link">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="E02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
